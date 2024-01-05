09d30f18d5069f457c31c7534ebc1c7b51675f01
Arash Altafi <arashaltafi1377@gmail.com>
2024-01-06 00:45:56 +0330

    a

--- sample2/server2.js
+++ sample2/server2.js
@@ -4,10 +4,39 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    res.write(('Hello World!'));
+    switch (req.url) {
+        case '/':
+            res.write(JSON.stringify({
+                value: 'Welcome To Home!',
+                status: 200
+            }));
+            break;
+        case '/about':
+            res.write(JSON.stringify({
+                value: 'Welcome To About!',
+                status: 200
+            }));
+            break;
+        case '/contact':
+            res.write(JSON.stringify({
+                value: 'Welcome To Contact!',
+                status: 200
+            }));
+            break;
+        default:
+            res.write(JSON.stringify({
+                value: 'Not Found!',
+                status: 404
+            }));
+            break;
+    }
     res.end();
 })
 
 server.listen(8080, () => {
     console.log('server is running at port 8080 ...');
 });
+
+server.on("connection", () => {
+    console.log('new user connected!');
+})
