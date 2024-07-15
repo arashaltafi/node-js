d26ef0829dbbd11e58f53599b151c20fc8f713ce
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-15 16:07:56 +0430

    fastify all method

--- sample30/server.js
+++ sample30/server.js
@@ -5,9 +5,81 @@ const fastify = Fastify({
   logger: true
 })
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+let items = [];
+
+// GET method
+fastify.get('/get', async (request, reply) => {
+  reply.status(200).send({
+    message: 'get method',
+    statusCode: 200,
+    data: items
+  });
+});
+
+// GET method Query
+fastify.get('/getQuery', async (request, reply) => {
+  const query = request.query
+  reply.status(200).send({
+    message: 'get method with query',
+    statusCode: 200,
+    query: query
+  });
+});
+
+// GET method Params
+fastify.get('/getParams/:id', async (request, reply) => {
+  const params = request.params
+  reply.status(200).send({
+    message: 'get method with params',
+    statusCode: 200,
+    params: params
+  });
+});
+
+// POST method
+fastify.post('/post', async (request, reply) => {
+  const item = request.body;
+  items.push(item);
+  reply.status(201).send({
+    message: 'post method',
+    statusCode: 201,
+    data: item
+  });
+});
+
+// DELETE method
+fastify.delete('/delete/:id', async (request, reply) => {
+  const { id } = request.params;
+  items = items.filter(item => item.id !== id);
+  reply.status(200).send({
+    message: 'delete method',
+    statusCode: 200,
+    data: items
+  });
+});
+
+// PATCH method
+fastify.patch('/patch', async (request, reply) => {
+  const { id } = request.query;
+  const updates = request.body;
+  let item = items.find(item => item.id === id);
+
+  if (item) {
+    item = { ...item, ...updates };
+    items = items.map(i => (i.id === id ? item : i));
+    reply.status(200).send({
+      message: 'delete method',
+      statusCode: 200,
+      data: item
+    });
+  } else {
+    reply.status(404).send({
+      message: 'delete method',
+      statusCode: 404,
+      data: 'Item not found'
+    });
+  }
+});
 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
