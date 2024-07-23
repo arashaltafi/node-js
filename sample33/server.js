792d5ff16eb3203f48598e5bd79543adce3161da
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-23 10:40:18 +0330

    apollo-server-express

--- sample33/server.js
+++ sample33/server.js
@@ -1,3 +1,39 @@
 // npm i apollo-server-express
 // npm i graphql-tools
 // https://www.apollographql.com/docs/apollo-server/
+
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Define your schema
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+// Define your resolvers
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+// Create an instance of ApolloServer
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Initialize an Express application
+const app = express();
+
+// Apply the Apollo GraphQL middleware and set the path to /graphql
+server.start().then(() => {
+    server.applyMiddleware({ app, path: '/graphql' });
+
+    // Define the port
+    const PORT = process.env.PORT || 4000;
+
+    // Start the server
+    app.listen(PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
